9937664313541246c0918e8aa06cd269930d4393
JpCardenasG <jpcardenasgonzalez.com>
2024-07-29 22:03:15 -0500

    feat: :sparkles: Se crea el componente de ProductCard y se agrega a la vista de Pedidos

---
+++ src/pages/Pedidos/ProductCard.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="tw-border tw-rounded-lg tw-p-4 tw-shadow-md tw-bg-slate-100">
+        <div class="tw-flex tw-justify-between tw-items-center">
+            <div>
+                <p class="tw-font-medium tw-text-black">Order #{{ order.id }}</p>
+                <p class="tw-font-normal tw-text-gray-500">{{ order.date }}</p>
+            </div>
+            <img :src="order.avatar" alt="Avatar" class="tw-w-10 tw-h-10 tw-rounded-full">
+        </div>
+        <div class="mt-4">
+            <div v-for="item in order.items" :key="item.name"
+                class="tw-flex tw-flex-col tw-justify-between tw-items-start tw-mb-4">
+                <div class="tw-flex tw-items-center">
+                    <img :src="item.image" alt="Item image" class="tw-w-10 tw-h-10 tw-mr-2">
+                    <div>
+                        <p class="tw-font-normal tw-text-black">{{ item.name }}</p>
+                        <p class="tw-font-normal tw-text-gray-500">{{ item.description }}</p>
+                    </div>
+                </div>
+                <div class="tw-flex tw-justify-between tw-w-full">
+                    <p class="tw-font-normal tw-text-black">${{ item.price }}</p>
+                    <p class="tw-text-black">Qty: {{ item.quantity }}</p>
+                </div>
+            </div>
+        </div>
+        <div class="tw-mt-4 tw-flex tw-justify-between tw-items-center">
+            <p class="tw-text-gray-500">{{ order.items.length }} Items</p>
+            <div>
+                <button v-if="order.status === 'completed'"
+                    class="tw-bg-green-100 tw-text-green-700 tw-px-2 tw-py-2 tw-rounded">COMPLETED</button>
+                <button v-if="order.status === 'rejected'"
+                    class="tw-bg-red-100 tw-text-red-700 tw-px-2 tw-py-2 tw-rounded">REJECTED</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        order: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
--- src/pages/views/Pedidos.vue
+++ src/pages/views/Pedidos.vue
@@ -1,13 +1,91 @@
 <template>
-<div>
-<h1 class="title">pedidos</h1>
-</div>
+    <div class="tw-grid tw-grid-cols-3 tw-gap-4">
+        <ProductCard v-for="order in orders" :key="order.id" :order="order" />
+    </div>
 </template>
 
-<script setup>
+<script>
+import ProductCard from "../Pedidos/ProductCard";
 
+const orders = [
+    {
+        "id": 351,
+        "date": "05 Feb 2023, 08:28 PM",
+        "avatar": "https://picsum.photos/50",
+        "items": [
+            {
+                "name": "Vegetable Mixups",
+                "description": "Vegetable Fritters with Egg",
+                "price": 5.30,
+                "quantity": 1,
+                "image": "https://images.pexels.com/photos/1143754/pexels-photo-1143754.jpeg"
+            },
+            {
+                "name": "Chinese Takeout Dish",
+                "description": "Fresh Prawn mix salad",
+                "price": 5.30,
+                "quantity": 1,
+                "image": "https://images.pexels.com/photos/5409015/pexels-photo-5409015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+        ],
+        "status": "completed"
+    },
+    {
+        "id": 350,
+        "date": "05 Feb 2023, 08:28 PM",
+        "avatar": "https://picsum.photos/50",
+        "items": [
+            {
+                "name": "Baked Pasted Dishes",
+                "description": "Vegetable Fritters with Egg",
+                "price": 2.48,
+                "quantity": 1,
+                "image": "https://images.pexels.com/photos/4109083/pexels-photo-4109083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            },
+            {
+                "name": "Chinese Takeout Dish",
+                "description": "Fresh Prawn mix salad",
+                "price": 5.30,
+                "quantity": 1,
+                "image": "https://images.pexels.com/photos/5409015/pexels-photo-5409015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+        ],
+        "status": "rejected"
+    },
+    {
+        "id": 349,
+        "date": "05 Feb 2023, 08:28 PM",
+        "avatar": "https://picsum.photos/50",
+        "items": [
+            {
+                "name": "Vegetable Mixups",
+                "description": "Vegetable Fritters with Egg",
+                "price": 5.30,
+                "quantity": 1,
+                "image": "https://images.pexels.com/photos/1143754/pexels-photo-1143754.jpeg"
+            },
+            {
+                "name": "Chinese Takeout Dish",
+                "description": "Fresh Prawn mix salad",
+                "price": 5.30,
+                "quantity": 1,
+                "image": "https://images.pexels.com/photos/5409015/pexels-photo-5409015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+        ],
+        "status": "completed"
+    }
+];
+
+export default {
+    components: {
+        ProductCard,
+    },
+    data() {
+        return {
+            orders: orders,
+        };
+    },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
