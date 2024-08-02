0353e57dbbce4cdc1aa0d44693638b6999d6a57a
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-08-02 07:27:08 -0500

    feat: :construction: Chat del bot terminado

--- src/pages/SitioWeb/MensajeDeTexto.vue
+++ src/pages/SitioWeb/MensajeDeTexto.vue
@@ -5,7 +5,7 @@
 
 <template>
 
-    <div class=" tw-mx-10 tw-flex">
+    <div class="message__container">
         <div class=" fw-h-15 fw-w-10">
 
             <div class=" tw-h-12 tw-w-12 tw-bg-orange-500 tw-rounded-full">
@@ -14,11 +14,32 @@
 
         </div>
 
-        <div class=" tw-text-stone-950">Mariana</div>
+        <div class="text__box">
+            <div class=" tw-max-h-50px ">Bot</div>
+            <div class=" tw-h-auto tw-bg-gray-400 tw-p-2 content__box">¿Que comercializas?</div>
+        </div>
     </div>
 
 </template>
 
-<style>
+<style scoped>
+
+.message__container {
+    display: flex;
+    max-width: 45%;
+    height: auto;
+    gap: 10px
+}
+
+.text__box {
+    width: 100%;
+    height: auto;
+}
+
+.content__box {
+    border-radius: 0px 5px 5px 5px;
+
+}
+
 
 </style>
